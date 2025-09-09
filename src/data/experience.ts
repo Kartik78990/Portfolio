996dd035dc39dec91c1e7bcfd0cc05c390db526e
Kartik Pundir <119773936+Kartik78990@users.noreply.github.com>
2025-09-09 12:09:07 +0530

    Update experience.ts

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -3,7 +3,7 @@ import { Experience } from '../types';
 export const experiences: Experience[] = [
   {
     id: 1,
-    role: 'Trainee',
+    role: 'Learner',
     company: 'Udemy',
     period: 'July, 2023 - August, 2023',
     description: 'Learnt the basic of C and C++ programming languages, focusing on syntax, data structures, and algorithms.',
@@ -11,10 +11,18 @@ export const experiences: Experience[] = [
   },
   {
     id: 2,
-    role: 'Intern Frontend Developer',
+    role: 'Trainee Frontend Developer',
     company: 'Solitaire Infosys',
     period: 'July, 2024 - August, 2024',
     description: 'Leading the frontend development team, architecting scalable React applications, and implementing design systems.',
     technologies: ['HTML', 'CSS', 'JavaScript', 'Bootstrap']
+  },
+  {
+    id: 2,
+    role: 'Intern Full Stack Developer',
+    company: 'CodeAlpha',
+    period: 'July, 2025 - October, 2025',
+    description: 'Applying my skills of Full Stack Developer to provide my contributions to various projects.',
+    technologies: ['Express', 'Node.js', 'React', 'Sequelize']
   },
 ];
